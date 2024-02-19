79a241bc4851d4a61a9dda500c4c0c426e7297e9
Nicolas Riss <48218773+nriss@users.noreply.github.com>
2024-02-19 18:15:56 +0100

    Update frcore.sh

--- frcore.sh
+++ frcore.sh
@@ -1,1 +1,3 @@
+git submodule update --init --recursive
+
 sudo java -Dfile.encoding=UTF-8 -jar input-cache/publisher.jar -go-publish  -source /Users/nicolasriss/Desktop/fr-ig/hl7.fhir.fr.core  -web  /Library/WebServer/Documents/InteropSante/IG-website-release/www/ig/fhir -registry /Library/WebServer/Documents/InteropSante/IG-website-release/ig-registry/fhir-ig-list.json -history /Library/WebServer/Documents/InteropSante/IG-website-release/ig-history -templates /Library/WebServer/Documents/InteropSante/IG-website-release/templates -temp /Library/WebServer/Documents/tmp
