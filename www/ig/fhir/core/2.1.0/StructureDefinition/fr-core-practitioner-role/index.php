ea1a8986b1956493511823280380e45c3c772f1d
Nicolas Riss <48218773+nriss@users.noreply.github.com>
2024-09-04 10:19:00 +0200

    add fr core 2.1.0

---
+++ www/ig/fhir/core/2.1.0/StructureDefinition/fr-core-practitioner-role/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.1.0/StructureDefinition-fr-core-practitioner-role.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.1.0/StructureDefinition-fr-core-practitioner-role.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.1.0/StructureDefinition-fr-core-practitioner-role.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.1.0/StructureDefinition-fr-core-practitioner-role.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.1.0/StructureDefinition-fr-core-practitioner-role.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.1.0/StructureDefinition-fr-core-practitioner-role.html');
+else 
+  Redirect('https://hl7.fr/ig/fhir/core/2.1.0/StructureDefinition-fr-core-practitioner-role.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
