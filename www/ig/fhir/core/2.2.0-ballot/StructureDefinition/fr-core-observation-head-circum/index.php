a1a99f9ea0a86626f0a7c9da868e069548347146
Nicolas Riss <48218773+nriss@users.noreply.github.com>
2025-11-17 13:45:39 +0100

    release fr core 2.2.0-ballot

---
+++ www/ig/fhir/core/2.2.0-ballot/StructureDefinition/fr-core-observation-head-circum/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-observation-head-circum.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-observation-head-circum.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-observation-head-circum.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-observation-head-circum.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-observation-head-circum.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-observation-head-circum.html');
+else 
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-observation-head-circum.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
---
+++ www/ig/fhir/core/2.2.0-ballot/StructureDefinition/fr-core-organization-number-of-theorical-accomadation-space/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-organization-number-of-theorical-accomadation-space.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-organization-number-of-theorical-accomadation-space.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-organization-number-of-theorical-accomadation-space.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-organization-number-of-theorical-accomadation-space.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-organization-number-of-theorical-accomadation-space.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-organization-number-of-theorical-accomadation-space.html');
+else 
+  Redirect('https://hl7.fr/ig/fhir/core/2.2.0-ballot/StructureDefinition-fr-core-organization-number-of-theorical-accomadation-space.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
